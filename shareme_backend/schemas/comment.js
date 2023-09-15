95974dedd676f84acdc3a2f2fb33ee5c3ea2228d
lakhdher <noureddine.bennejma@insat.u-carthage.tn>
2023-09-15 13:31:07 +0100

    initial commit

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'comment',
+    type: 'object',
+    title: 'Comment',
+    fields : [
+        {
+            name : "postedBy",
+            type : "postedBy",
+            title : "Posted By"
+        },
+        {
+            name : "comment",
+            type : "string",
+            title : "Comment"
+        }
+    ]
+}
---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    type: 'object',
+    title: 'Save',
+    fields : [
+        {
+            name : "postedBy",
+            type : "postedBy",
+            title : "Posted By"
+        },
+        {
+            name : "userId",
+            type : "string",
+            title : "UserId"
+        }
+    ]
+}
